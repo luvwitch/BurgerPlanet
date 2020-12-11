f2af6645397e3abf95500b164ef9bdfdd023b4f4
luvwitch <itsalexkelly@gmail.com>
2020-12-11 15:01:08 -0800

    connection changed to include jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: process.env.host,
-  port: process.env.port,
-  user: process.env.username,
-  password: process.env.password,
-  database: process.env.database
-});
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.username,
+    password: process.env.password,
+    database: process.env.database
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
